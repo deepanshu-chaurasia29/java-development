f77a3b4a98542c82c0f0f77571b0a3aca2d5af5a
Deepanshu Chaurasia <deepanshustm2003@gmail.com>
2026-08-09 03:28:24 +0530

    Second commit

--- HibernateLearning/src/main/java/com/deepu/model/Student.java
+++ HibernateLearning/src/main/java/com/deepu/model/Student.java
@@ -19,6 +19,13 @@ public class Student
 	@Column(name="SCITY")
 	private String sCity;
 	
+	public Student(Integer sId, String sName, String sCity) {
+		super();
+		this.sId = sId;
+		this.sName = sName;
+		this.sCity = sCity;
+	}
+
 	public Student(){
 		System.out.println("Student class constructor");
 	}
